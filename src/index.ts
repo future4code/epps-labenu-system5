4d4324000caf211391a7ad240ec71438d2673bab
Raiza Leal Franca <raizale@hotmail.com>
2021-04-02 10:32:32 -0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+import knex from 'knex';
+import dotenv from 'dotenv';
+import app from './app'
+import connection from './connection'
+
+
+
